2482218e9bfd6a89af9d79ad9e020b2c289a616f
Diego Honório <diegohonoriiio@gmail.com>
2025-07-28 03:26:30 -0300

    Refactoring the code
    Refactoring the code in the gastos_repositori.py file, inserting the logic with with.

--- src/database/gasto_repository.py
+++ src/database/gasto_repository.py
@@ -3,79 +3,72 @@ from src.database.connection import get_connection
 
 
 def criar_tabela(): # Cria a tabela gastos para armazenar os dados.
-    conn = get_connection()
-    cursor = conn.cursor()
+    with get_connection() as conn:
+        cursor = conn.cursor()
 
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS gastos (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            valor REAL NOT NULL,
-            categoria TEXT,
-            descricao TEXT,
-            data TEXT
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS gastos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                valor REAL NOT NULL,
+                categoria TEXT,
+                descricao TEXT,
+                data TEXT
         )
-    """)
+        """)
 
-    conn.commit()
-    cursor.close()
-    conn.close()
+        conn.commit()
+
 
 
 def inserir_gasto(gasto): # insere os valores informados pelo usuario a tabela gastos 
-    conn = get_connection()
-    cursor = conn.cursor()
+    with get_connection() as conn:
+        cursor = conn.cursor()
 
-    cursor.execute(
-        "INSERT INTO gastos (valor, categoria, descricao, data) VALUES (?, ?, ?, ?)", 
-        (gasto.valor, gasto.categoria, gasto.descricao, gasto.data)
-    )
+        cursor.execute(
+            "INSERT INTO gastos (valor, categoria, descricao, data) VALUES (?, ?, ?, ?)", 
+            (gasto.valor, gasto.categoria, gasto.descricao, gasto.data)
+        )
 
-    conn.commit()
+        conn.commit()
 
-    cursor.close()
-    conn.close()
 
 
 def excluir_gastos(id): # exclui o gasto com base no ID informado pelo usuario 
-    conn = get_connection()
-    cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM gastos WHERE id = ?", (id,))
-    resultado = cursor.fetchone()
+    with get_connection() as conn:
+        cursor = conn.cursor()
 
-    if resultado is None:
-        print("Nenhum gasto encontrado com esse ID.")
-    else:
-        cursor.execute(" DELETE FROM gastos WHERE id = ?", (id,))
-        conn.commit()
-        print(f"Gasto com ID {id} foi excluído com sucesso.")
+        cursor.execute("SELECT * FROM gastos WHERE id = ?", (id,))
+        resultado = cursor.fetchone()
 
-
-    cursor.close()
-    conn.close()
+        if resultado is None:
+            print("Nenhum gasto encontrado com esse ID.")
+        else:
+            cursor.execute(" DELETE FROM gastos WHERE id = ?", (id,))
+            conn.commit()
+            print(f"Gasto com ID {id} foi excluído com sucesso.")
 
 
 
 def editar_gastos(): # Edita o gasto criado pelo usuario
-    conn = get_connection()
-    cursor = conn.cursor()
-    pass
+    with get_connection() as conn:
+
+        cursor = conn.cursor()
+        pass
 
 
 
 
 
 def listar_gastos(): # Lista os gastos que estão no banco de dados do usuario.
-    conn = get_connection()
-    cursor = conn.cursor()
+    with get_connection() as conn:
+        cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM gastos")
-    resultados = cursor.fetchall()
+        cursor.execute("SELECT * FROM gastos")
+        resultados = cursor.fetchall()
 
 
-    for id, valor, categoria, descricao, data in resultados:
-        print(f"ID: {id} Valor: R${valor}, Categoria: {categoria}, Descrição: {descricao}, Data: {data} ")
+        for id, valor, categoria, descricao, data in resultados:
+            print(f"ID: {id} Valor: R${valor}, Categoria: {categoria}, Descrição: {descricao}, Data: {data} ")
 
-    cursor.close()
-    conn.close()
-    return resultados
+        return resultados
