fccb19227e0fc61bb9aea82da3ef419a01f3b048
Diego Honório <diegohonoriiio@gmail.com>
2025-08-13 08:57:37 -0300

    visual creation of menu screens.
    Screen Creation
    - Header
    - Menu_Choose
    - Menu_Login
    
    Remember that the entire visual part of the program will be in CLI

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from src.models.gastos import Gasto
 from src.models.utils.input_utils import coletar_dados_edicao
 from src.database.gasto_repository import inserir_gasto, criar_tabela, listar_gastos, excluir_gastos, editar_gastos, filtrar_gastos_data, calcular_gastos
-from src.views.menu import menu
+from src.views.menu import cabecalho, menu_escolha, menu_login
 
 def main():
-    menu()
+    cabecalho()
     decisao = input('Filtrar Gastos? ').upper()
     if decisao == "SIM":
         inicio = input('inicio: ')
--- src/views/menu.py
+++ src/views/menu.py
@@ -1,13 +1,38 @@
-from src.views.colors import *
+from colors import Cores
+from getpass import getpass
 
 PRETO, VERMELHO, VERDE, AMARELO, AZUL, MAGENTA, CIANO, BRANCO,PRETO_CLARO, VERMELHO_CLARO, VERDE_CLARO, AMARELO_CLARO, AZUL_CLARO, MAGENTA_CLARO, CIANO_CLARO, BRANCO_CLARO, RESET = Cores()
 
 versao = '1.0.1-V1'
-def menu():
+def cabecalho(): # --> cabeçalho principal do programa
     print('=' * 60)
     print(f'{VERDE}WALLETO{RESET}{AZUL} - {versao}{RESET}'.center(60))
     print('=' * 60)
 
+
+def menu_escolha(): # --> menu principal do programa, que levara o usuario onde ele desejar 
+    print('=' * 60)
+    print('[1] - Ver gastos')
+    print('[2] - Adicionar gastos')
+    print('[3] - Editar Gasto')
+    print('[4] - Excluir Gasto')
+    print('[5] - Sair')
+    print('=' * 60)
+
+    opc = input('Escolha: ')
+
+    return opc
+
+
+def menu_login(): # --> menu criado para o login do usuario, em breve irei implementar o login 
+    print('=' * 60)
+    print('LOGIN WALLETO'.center(60))
+    print('=' * 60) 
+    usuario = input('Informe o usuario ou email: ')
+    senha = getpass('Digite sua senha: ') # --> oculta a senha informada pelo usuarik
+
+
+    return usuario, senha
 
 
 
